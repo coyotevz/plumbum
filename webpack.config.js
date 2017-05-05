4fe60b919f261020420eae64015040ba75959709
Augusto Roccasalva <augusto@rioplomo.com.ar>
2017-05-05 01:53:16 -0300

    Some more work on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,51 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var AutoPrefixerPlugin = require('autoprefixer')
+var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin')
 
-var extractSass = new ExtractTextPlugin({
-  filename: "[name].[contenthash].css",
-  disable: process.env.NODE_ENV === "devlopment",
-});
+// Environment detection
+var node_env = process.env.NODE_ENV || 'development'
+
+var rootAssetPath = './plumbum/static'
+var buildOutPath = path.resolve(__dirname, 'build/public')
+
+var plugins = [
+  new webpack.NoEmitOnErrorsPlugin(),
+  new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js'}),
+  new ExtractTextPlugin('[name].[hash:7].css'),
+  new ManifestRevisionPlugin(path.join('build', 'manifest.json'), {
+    rootAssetPath: rootAssetPath,
+    ignorePaths: ['/js', '/css', '/scss'],
+  })
+]
+
+if (node_env !== 'development') {
+   plugins = (plugins || []).concat([
+    new webpack.optimize.UglifyJsPlugin({compressor: { warnings: false }}),
+  ])
+}
 
 module.exports = {
   entry: {
-    app_js: [
-      './plumbum/static/js/jquery-3.2.1.slim.js',
+    vendor: [
+      rootAssetPath + '/js/vendor/jquery-3.2.1.slim.js',
+      rootAssetPath + '/js/vendor/tether.js',
+      rootAssetPath + '/js/vendor/bootstrap.js',
+      rootAssetPath + '/js/vendor/axios.js',
     ],
-    app_css: './plumbum/static/scss/plumbum.scss',
+    script: [
+      rootAssetPath + '/js/app/plumbum.js',
+    ],
+    style: [
+      rootAssetPath + '/scss/plumbum.scss',
+    ]
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/static/',
-    filename: '[name].[chunkhash].js',
+    path: buildOutPath,
+    publicPath: 'http://localhost:2992/assets/',
+    filename: '[name].[hash:7].js',
+    chunkFilename: '[id].[hash:7].js',
   },
   resolve: {
     extensions: ['.js', '.css'],
@@ -27,21 +55,39 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'script-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+          fallback: 'style-loader'
+        }),
       },
       {
         test: /\.scss$/,
-        use: extractSass.extract({
-          use: [
-            { loader: "css-loader" },
-            { loader: "sass-loader" },
-          ],
-          fallback: "style-loader",
-        })
+        loader: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 100000,
+          name: 'img/[name].[hash:7].[ext]',
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 100000,
+          name: 'fonts/[name].[hash:7].[ext]',
+        }
       },
     ]
   },
-  plugins: [
-    extractSass,
-  ]
-};
+  plugins: plugins,
+}
